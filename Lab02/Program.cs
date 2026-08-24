ac86210283b4c841ac38216fea655802dcb6ebbf
kawin123-hub <kkty4846@gmail.com>
2026-08-24 16:26:43 +0700

    Add Lab02 Header

---
+++ Lab02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID :Kawinyarat Sakprapakorn
+ * Name       :Lab02
+ * Section    :129B
+ * No.        :N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
